a303acd9599358fdf82c1ab42d8acabe6f94bfbe
Ria Vora <riavora@fb.com>
2020-06-25 09:00:17 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Ria Vora on 6/25/20.
+//  Copyright © 2020 Ria Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
